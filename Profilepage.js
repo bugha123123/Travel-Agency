c30d8b07599733df5ccc5bd9815bb51bb8c81d87
bugha123123 <irakliberdzena314@gmail.com>
2023-12-27 23:37:25 +0400

    Profile not working

--- Profilepage.js
+++ Profilepage.js
@@ -1,23 +1,22 @@
 const Name = document.querySelector(".name")
  const logoutButton = document.querySelector(".logout")
 
-const UserName = localStorage.getItem("UserName")
 
-if (UserName) {
-  Name.innerHTML = UserName;
-}else{
-  Name.innerHTML = "User"
-}
 
 logoutButton.addEventListener("click", () => {
   // Clear user name from local storage and display
   window.localStorage.removeItem("UserName");
-  userNameDisplay.innerHTML = "";
+
 });
 
+const updateProfileUI = () => {
+  const UserName = localStorage.getItem("UserName");
 
-
-
+  if (UserName) {
+    Name.innerHTML = UserName;
+  } else {
+    Name.innerHTML = "User";
+  }
 
 if (localStorage.getItem("UserName")) {
   document.querySelector(".googleicon").style.display = "block"
@@ -39,43 +38,68 @@ if (localStorage.getItem("UserName")) {
      logoutButton.style.display = "none"
  }
 
-
- const LOCALSTORAGEEMAILADDRESS = localStorage.getItem("EmailAddress")
-
-const InputUserName = document.querySelector(".editUserName")
-
-const InputEmailAddress = document.querySelector(".editEmailAddress")
-
-const InputPassword = document.querySelector(".editPassword")
+};
 
 
 
-async function UpdateProfileData(NewUserName, NewEmailAddress, NewPassword){
 
-const body = {
-  UserName:NewUserName,
-  EmailAddress:NewEmailAddress,
-  Password: NewPassword
-}
 
- await fetch(`http://localhost:5161/api/Profile/${LOCALSTORAGEEMAILADDRESS}`,{
 
-    method: "PUT",
-    body: JSON.stringify(body),
-    headers: {
-        "Content-Type": "application/json"
+ const LOCALSTORAGEEMAILADDRESS = localStorage.getItem("EmailAddress");
+
+ const InputUserName = document.querySelector(".editUserName");
+ const InputEmailAddress = document.querySelector(".editEmailAddress");
+ const InputPassword = document.querySelector(".editPassword");
+ const UpdateProfileData = async (NewUserName, NewEmailAddress, NewPassword) => {
+  const body = {
+    UserName: NewUserName,
+    EmailAddress: NewEmailAddress,
+    Password: NewPassword,
+  };
+
+  try {
+    const response = await fetch(`http://localhost:5161/api/user/${LOCALSTORAGEEMAILADDRESS}`, {
+      method: "PUT",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-  })
-  .then(resp => resp.json()).then(data => console.log(data))
+    // Update local storage with new user details
+    localStorage.setItem("UserName", NewUserName);
+    localStorage.setItem("EmailAddress", NewEmailAddress);
 
+    // Dynamically update profile name
+    Name.innerHTML = NewUserName;
 
- }
+    // Update UI elements based on user profile
+    updateProfileUI();
+
+  } catch (error) {
+    console.error("Error updating profile:", error);
+
+    // Log additional information about the error
+    console.log("Response status:", error.response?.status);
+    console.log("Response body:", error.response?.body);
+
+    // Handle the error as needed (e.g., display a user-friendly message)
+  }
+};
 
 document.querySelector(".update-profile-button").addEventListener("click", () => {
+  UpdateProfileData(
+    InputUserName.value,
+    InputEmailAddress.value,
+    InputPassword.value
+  );
+});
 
-  UpdateProfileData(InputUserName.value,InputEmailAddress.value, InputPassword )
 
-
-})
+// Call this function on page load to ensure the UI reflects the initial user profile
+updateProfileUI();
 
